46883cc8964e1db6b84e5b63e6249772157ae591
krest2020 <18635572970@163.com>
2022-06-29 15:15:03 +0800

    delete

--- krest-rpc/src/main/java/com/krest/rpc/common/RpcInvokeHook.java
+++
@@ -1,8 +0,0 @@
-package com.krest.rpc.common;
-
-public interface RpcInvokeHook {
-
-    void beforeInvoke(String methodName, Object[] args);
-
-    void afterInvoke(String methodName, Object[] args);
-}
