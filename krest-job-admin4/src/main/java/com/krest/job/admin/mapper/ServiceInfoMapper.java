a803fbf66520f80f3892d30f81e6a4b86279f1ff
krest2020 <18635572970@163.com>
2022-07-07 12:14:38 +0800

    '7.7'

--- krest-job-admin4/src/main/java/com/krest/job/admin/mapper/ServiceInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.krest.job.admin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.krest.job.common.entity.ServiceInfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ServiceInfoMapper extends BaseMapper<ServiceInfo> {
-}
